1bafb2054d39746700030bfcc4b82282bd33939b
Mitsuoka Takahiro <tmitsuoka0423@gmail.com>
2024-02-13 05:40:26 +0000

    feat: add morgan

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
 const app = express();
 
+console.log('launching...');
+
+app.use(morgan('combined'));
 app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), (req, res) => {
+  console.log(res);
+  
   const interaction = req.body;
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     res.send({
